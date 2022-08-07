22a877fc83419bed1a6a6d176d7db748f3c71e9f
Shikhar Gupta <shikhargupta013@gmail.com>
2022-08-07 11:30:45 +0530

    dropdown updates

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -4,29 +4,23 @@ import {Link} from 'react-router-dom'
 
 function DefaultLayout(props) {
     const user = JSON.parse(localStorage.getItem('user'))
+    console.log(user.username);
+    
+  
   const menu = (
     <Menu>
         <Menu.Item>
-        <a
-         
-          href="/"
-        >
+        <a href="/" >
           Home
         </a>
       </Menu.Item>
       <Menu.Item>
-        <a
-          
-          href="/userbookings"
-        >
+        <a href="/userbookings">
           Bookings
         </a>
       </Menu.Item>
       <Menu.Item>
-        <a
-         
-          href="/admin"
-        >
+        <a href="/admin"  >
           Admin
         </a>
       </Menu.Item>
@@ -38,6 +32,31 @@ function DefaultLayout(props) {
       </Menu.Item>
     </Menu>
   );
+  const menu1 = (
+    <Menu>
+        <Menu.Item>
+        <a href="/" >
+          Home
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a href="/userbookings">
+          Bookings
+        </a>
+      </Menu.Item>
+      
+      <Menu.Item onClick={()=>{
+          localStorage.removeItem('user');
+          window.location.href='/login'
+      }}>
+          <li style={{color:'orangered'}}>Logout</li>
+      </Menu.Item>
+    </Menu>
+  );
+  var drop = menu1;
+    if(user.username==="admin"){
+      drop = menu;
+    }
   return (
     <div>
       <div className="header bs1">
@@ -45,10 +64,18 @@ function DefaultLayout(props) {
               <Col lg={20} sm={24} xs={24}>
               <div className="d-flex justify-content-between">
              <h1 ><b><Link to='/' style={{color:'orangered'}}>Rent-O-Cars</Link></b></h1>
-
-          <Dropdown overlay={menu} placement="bottomCenter">
+{/*           
+             <Button onclick={()=>{
+                if(user.username === "admin")
+                  return <Dropdown overlay={menu} placement="bottomCenter"></Dropdown>
+                else
+                  return <Dropdown overlay={menu1} placement="bottomCenter"></Dropdown>
+             }}>{user.username}</Button> */}
+          
+          <Dropdown overlay={drop} placement="bottomCenter">
             <Button>{user.username}</Button>
           </Dropdown>
+         
         </div>
               </Col>
           </Row>
@@ -69,5 +96,6 @@ function DefaultLayout(props) {
     </div>
   );
 }
+
 
 export default DefaultLayout;
